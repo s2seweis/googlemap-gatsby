d5b621349a40f1b9bfb26e15972d61f4e42a8526
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-29 12:56:45 +0200

    upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,18 +44,18 @@ module.exports = {
       options: {
 
         // local
-          // apiURL: "http://127.0.0.1:1337",
+          apiURL: "http://127.0.0.1:1337",
 
 
 
         // deployed
-        apiURL: 'https://strapi4-8-2.herokuapp.com',
+        // apiURL: 'https://strapi4-8-2.herokuapp.com',
 
         //delpoyed
-        accessToken: '9a09cc791f40d7aaed5a6e4536b2c7fac9dc6b04521a59fb34b342ef7c1bc8d38664d64b888e78db09fea3ccd46c1eebd984d27929a7ef1ae1f3de03b83789c64e4cfd26bcff79375e6c32a4445ce0ae1f024b5c03f0c1826111c888b7fd734405f6c46a2a617fd522e44583b744f8b7bc9bdd4aa3dfd7a969144c23023d1ddb',
+        // accessToken: '9a09cc791f40d7aaed5a6e4536b2c7fac9dc6b04521a59fb34b342ef7c1bc8d38664d64b888e78db09fea3ccd46c1eebd984d27929a7ef1ae1f3de03b83789c64e4cfd26bcff79375e6c32a4445ce0ae1f024b5c03f0c1826111c888b7fd734405f6c46a2a617fd522e44583b744f8b7bc9bdd4aa3dfd7a969144c23023d1ddb',
 
         //local
-        // accessToken: "17b048a03fd80741931d6a4ef6f6877b808d90b5fe095303a96338ca429bcbe3e0ac5fc1a19700420e2b3c38fdccd3e4b03f54f3df305eecade9b8cac13434e0c9314040f0ad83c0e180bb3fe2ea984351f409d85e4e8ee58ee2d28232b15015a390bc8a54e18824506e17c3a1c6cac4320b39895aa808a3012252ae2da06929",
+        accessToken: "4ad18d9ec2b2c9fcb2d26c3c4031e8c938b6c79d14b46a457eee54c17b808baaa759e1e0f9c54dcd524ce373550f0424b2d5ee8541be3a9fcbe357754d151504db4a8fe1fd934658cb47b9e47218198e3a956082e461d39c7c203fca2cd8d6049a31abee946f9ee9e2c2f7a00c4aab6b23276af12556cc311fbfce4f5c558b79",
         
         
         collectionTypes: [
