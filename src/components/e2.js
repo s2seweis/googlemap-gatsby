7529f6e9845b5d7098cec3a96bd238b28ae7a002
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-31 23:52:38 +0200

    test:1 | gmap api

--- src/components/e2.js
+++ src/components/e2.js
@@ -1,239 +1,111 @@
 import React from 'react';
-// import {Link} from 'gatsby';
-// import {StaticImage} from 'gatsby-plugin-image';
+import Seo from "../components/seo"
 
-// import group from '../assets/images/group.png';
+import Layout from '../components/layout';
 
-import {use} from '../styles/main-use.css';
+const E1 = () => {
+  // const seo = {
+  //     metaTitle: title,
+  //     metaDescription: title,
+  //   }
 
-// import image1 from '../assets/images/image1.jpg';
-
-// import {main} from '../styles/main.css';
-
-import {FaFacebookSquare} from 'react-icons/fa';
-import {FaInstagram} from 'react-icons/fa';
-import {FaTiktok} from 'react-icons/fa';
-import {FaYoutube} from 'react-icons/fa';
-
-// import AdBlock from './adblock';
-
-// import {GatsbyImage, getImage} from 'gatsby-plugin-image';
-
-const E2 = () => {
   return (
-    <header className="e2" style={{}}>
+    <div>
 
-      {/* <AdBlock/> */}
+      <section className="contact-page">
 
-      <section className="e2-level1">
+      <Seo seo={{metaTitle: 'Contact'}} />
 
-        <div className="e2-grid">
 
-          {/* <h1>E2</h1> */}
-
-          <div
-            className="e2-box1"
+        <div style={{marginTop: '20px'}}>
+          <a
             style={{
-              backgroundImage: 'url(https://food-blog-v001-image-bucket.s3.eu-central-1.amazonaws.com/small_mitzi_falmarin_831067a945.webp',
+              fontSize: '1.3rem',
+              padding: '5px',
+              marginLeft: '20px',
             }}
+            href="javascript:history.back()"
           >
-            {' '}
-            <div
-              style={{
-                marginTop: '110%',
-                textAlign: 'center',
-                display: 'flex',
-                justifyContent: 'space-between',
-                fontSize: '2rem',
-              }}
-              className="<e2-box-social-links>"
-            />
-            {' '}
-          </div>
-          <div className="e2-box2" style={{padding: '15px'}}>
-
-            <p
-              className="e2-text-p2"
-              style={{
-                textAlign: 'center',
-                // fontSize: '2rem',
-                fontFamily: 'Lobster Two',
-                marginTop: '20px',
-              }}
-            >
-              Welcome to my food blog
-            </p>
-
-            <div
-              className="e2-text"
-              style={{
-                margin: 'auto',
-                height: '60%',
-                alignItems: 'center',
-                display: 'grid',
-                // fontSize: '1.2rem',
-                padding: '20px',
-                fontFamily: 'Lobster Two',
-              }}
-            >
-
-              <p className="e2-text-p1" style={{}}>
-                where I share my passion for cooking and all things food-related! My name is Mitzi Falmarin, and I'm thrilled to have you here.
-                {' '}
-              </p>
-
-              <p style={{fontSize: '1.4rem', textAlign: 'center'}}>
-                {' '}Check ou my other Channels{' '}
-              </p>
-
-              <div
-                style={{
-                  display: 'flex',
-                  fontSize: '2rem',
-                  justifyContent: 'space-around',
-                }}
-                className="flex-social-links"
-              >
-
-                <a
-                  aria-label="link"
-                  className="link-social-desktop"
-                  // style={{color: 'white'}}
-                  href="https://www.facebook.com/people/The-Solace-Life/100090725413897/?mibextid=LQQJ4d"
-                >
-                  <span><FaFacebookSquare /></span>
-                </a>
-                <a
-                  aria-label="link"
-                  // style={{color: 'white'}}
-                  className="link-social-desktop"
-                  href="https://www.instagram.com"
-                >
-                  <span><FaInstagram /></span>
-                </a>
-                <a
-                  aria-label="link"
-                  // style={{color: 'white'}}
-                  className="link-social-desktop"
-                  href="https://www.tiktok.com"
-                >
-                  <span><FaTiktok /></span>
-                </a>
-                <a
-                  aria-label="link"
-                  // style={{color: 'white'}}
-                  className="link-social-desktop"
-                  href="https://www.youtube.com/@Thesolacelife"
-                >
-                  <span><FaYoutube /></span>
-                </a>
-
-              </div>
-              {/* <p></p> */}
-
-            </div>
-
-          </div>
-
+            <button type="button" className="btn btn-link">Go Back</button>
+          </a>
         </div>
 
-        <div className="e2-flex">
-
-          {/* <h1>E2</h1> */}
-
-          <div
-            className="e2-box1-flex"
-            style={{
-              backgroundImage: 'url(https://food-blog-v001-image-bucket.s3.eu-central-1.amazonaws.com/small_mitzi_falmarin_831067a945.webp)',
-            }}
+        <article
+          className="contact-form"
+          style={{margin: 'auto', marginTop: '100px'}}
+        >
+          <h3 style={{textAlign: 'center'}}>get in touch</h3>
+          <form
+            action="https://formsubmit.co/weissenborn.sebastian@gmail.com"
+            method="POST"
           >
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input type="hidden" name="_captcha" value="false" />
+              <input
+                type="hidden"
+                name="_next"
+                value="https://precious-conkies-b8586b.netlify.app/thankyou"
+              />
 
-            <div
-              style={{
-                marginTop: '100%',
-                display: 'flex',
-                // justifyContent: 'center',
-                width: '100%',
-                justifyContent: 'space-between',
-                fontSize: '2rem',
-              }}
-              className="e2-box1-flex-social-links"
-            >
-              <a
-                aria-label="link"
-                // style={{color: 'black'}}
-                href="https://www.facebook.com/people/The-Solace-Life/100090725413897/?mibextid=LQQJ4d"
-                className="link-social-mobile"
-              >
-                <span><FaFacebookSquare /></span>
-              </a>
-              <a
-                aria-label="link"
-                // style={{color: 'black'}}
-                href="https://www.instagram.com"
-                className="link-social-mobile"
-              >
-                <span><FaInstagram /></span>
-              </a>
-              <a
-                aria-label="link"
-                // style={{color: 'black'}}
-                href="https://www.tiktok.com"
-                className="link-social-mobile"
-              >
-                <span><FaTiktok /></span>
-              </a>
-              <a
-                aria-label="link"
-                // style={{color: 'black'}}
-                href="https://www.youtube.com/@Thesolacelife"
-                className="link-social-mobile"
-              >
-                <span><FaYoutube /></span>
-              </a>
+              <input
+                type="hidden"
+                name="_subject"
+                value="Message From My Portfolio!!!"
+              />
 
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+                required
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              />
             </div>
-
-          </div>
-
-          <div style={{marginTop: '40px'}} className="e2-box2-flex">
-
-            <div
-              style={{margin: '0px 15px 0px 15px', background: 'antiquewhite'}}
-              className="e2-box2-flex-text"
+            <button
+              style={{backgroundColor: 'lightblue'}}
+              type="submit"
+              className="submit-btn btn"
             >
-
-              <div
-                style={{
-                  textAlign: 'center',
-                  padding: '20px 20px 20px 20px',
-                  fontSize: '2rem',
-                  fontFamily: 'Lobster Two',
-                }}
-              >
-                Welcome to my food blog
-              </div>
-
-              <div
-                style={{
-                  textAlign: 'center',
-                  padding: '20px 20px 20px 20px',
-                  fontSize: '1.5rem',
-                  fontFamily: 'Lobster Two',
-                }}
-              >
-                where I share my divassion for cooking and all things food-related! My name is Mitzi, and I'm thrilled to have you here.
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-
+              submit here
+            </button>
+          </form>
+        </article>
       </section>
-    </header>
+
+      <div
+          className="ui breadcrumb"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            // marginBottom: '50px',
+            marginTop: '50px',
+            paddingBottom:"50px"
+          }}
+        >
+          <a aria-label="link" className="section" href="/news">News</a>
+
+          <div className="divider">/</div>
+          <a aria-label="link" className="section" href="/about">About</a>
+
+          <div className="divider">/</div>
+          <a aria-label="link" className="section " href="/index-all-recipes">Index</a>
+          {/* className="active section " */}
+        </div>
+
+    </div>
   );
 };
 
-export default E2;
+export default E1;
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Overview from '../components/overview';
 // import Seperate from '../components/seperate';
 import Content3 from '../components/content3';
-import E2 from '../components/e2';
+import E1 from '../components/e2';
 
-import ScrollToTop from "react-scroll-to-top";
+import ScrollToTop from 'react-scroll-to-top';
+
+// ###
+
+import {GoogleMap, Marker, useLoadScript} from '@react-google-maps/api';
+import {useMemo} from 'react';
+
+// ###
 
 const IndexPage = () => {
+  const {isLoaded} = useLoadScript ({
+    // googleMapsApiKey: "AIzaSyDQQQ0-nbHRT6gkbtMlaVRgxawHwLITyGI",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+
+  const center = useMemo (() => ({lat: 18.52043, lng: 73.856743}), []);
+
   const {allStrapiArticle, strapiGlobal} = useStaticQuery (graphql`
     query {
       allStrapiArticle {
@@ -36,24 +50,40 @@ const IndexPage = () => {
     }
   `);
 
-  console.log("line:1",strapiGlobal );
-
-
+  console.log ('line:1', strapiGlobal);
 
   return (
     <Layout>
 
-      <Seo 
-      seo={{metaTitle: 'Home'}}
-       />
+      <Seo seo={{metaTitle: 'Home'}} />
 
-      <ScrollToTop smooth color="#708d3e" style={{display:"flex", justifyContent:"center", alignItems:"center"}} />
+      <ScrollToTop
+        smooth
+        color="#708d3e"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      />
 
       <Hero />
       {/* <Seperate /> */}
       {/* <Hero1 /> */}
       {/* <Seperate /> */}
-      {/* <E2 /> */}
+
+      <E1 />
+
+      <div className="App">
+        <h1>GoogleMap API</h1>
+        {!isLoaded
+          ? <h1>Loading...</h1>
+          : <GoogleMap
+              mapContainerClassName="map-container"
+              center={center}
+              zoom={10}
+            />}
+      </div>
 
       {/* <Headings
         title={strapiGlobal.siteName}
